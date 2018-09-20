b3ad395658406a7f297c2fbf6df06ec1a34934d2
sarahfmc <sarahfmc@so1819>
2018-09-20 21:50:43 +0100

    Projeto cliente criado para testar o git :)

---
+++ Cliente/main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: sarahfmc
+ *
+ * Created on September 20, 2018, 9:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("O Ricardo e um fixe!\n");
+
+    return (EXIT_SUCCESS);
+}
+
