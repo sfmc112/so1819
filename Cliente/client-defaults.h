e380485a5b17ee573f5c7785bbd58b2739b3d8e7
Sarah Cunha <missntc.sf@gmail.com>
2018-10-11 12:23:30 +0100

    client, server and medit header files, script to get .c and .h to a separate folder

---
+++ Cliente/client-defaults.h
@@ -0,0 +1,7 @@
+#ifndef CLIENT_DEFAULTS_H
+#define CLIENT_DEFAULTS_H
+
+#include "../medit-defaults.h"
+
+#endif /* CLIENT_DEFAULTS_H */
+
---
+++ medit-defaults.h
@@ -0,0 +1,9 @@
+#ifndef MEDIT_DEFAULTS_H
+#define MEDIT_DEFAULTS_H
+
+#define VAR_MAXLINES 15
+#define VAR_MAXCOLUMNS 45
+
+//Definição das estruturas de dados
+
+#endif
