7e24915318b7cbb7c3db0e6651b6e9dc7b6120c7
ricardo <ricardo@debian>
2018-10-28 01:20:23 +0000

    Navegação e edição criados!
    Modo de edição está com bugs!

--- Cliente/client-functions.c
+++ Cliente/client-functions.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
-
+// OLD VERSION
+/*
 #define X_INDEX 5
 
 void resetLine(char *text);
@@ -18,6 +19,36 @@ void backSpaceKey(int posy, int posx, int ncol);
 void deleteKey(int posy, int posx, int ncol);
 int moveAllToTheRight(int posy, int posx, int ncol);
 void getTextoDaLinha(char* texto, int linha, int maxX);
+ */
+
+#define TITLE "MEDIT EDITOR--------------------filename.xpto-----------------------------------"
+#define WIN_EDITOR_MAX_X 45
+#define WIN_EDITOR_MAX_Y 15
+#define WIN_USER_MAX_X 8
+#define WIN_USER_MAX_Y WIN_EDITOR_MAX_Y
+#define WIN_LINENUM_MAX_X 2
+#define WIN_LINENUM_MAX_Y WIN_EDITOR_MAX_Y
+#define WIN_TITLE_MAX_X COLS
+#define WIN_TITLE_MAX_Y 1
+
+WINDOW* createSubWindow(WINDOW* janelaMae, int dimY, int dimX, int startY, int startX);
+void configureWindow(WINDOW* janela, int setCores);
+void writeLineNumbers();
+void writeUser(char* name, int line);
+void writeDocument(char* text[], int nLines);
+void writeTextLine(char* text, int line);
+void clearEditor(int dimY, int dimX);
+void resetLine(int line, int dimX);
+void writeTitle();
+void refreshCursor(int y, int x);
+void editMode(int y, int x);
+void writeKey(int key, char* linha, int x);
+void getLinha(char* linha, int y);
+//WINDOW* masterWin;
+WINDOW* titleWin;
+WINDOW* userWin;
+WINDOW* lineWin;
+WINDOW* editorWin;
 
 /**
  * Verifica se ao inicializar o programa do cliente foi introduzido algum argumento.
@@ -58,6 +89,250 @@ void loginSession() {
  * Função responsável por tudo acerca do editor.
  */
 void editor() {
+    initscr();
+    start_color();
+    clear();
+    cbreak();
+    keypad(stdscr, TRUE);
+    noecho();
+
+    init_pair(1, COLOR_BLACK, COLOR_CYAN);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+
+    titleWin = createSubWindow(stdscr, WIN_TITLE_MAX_Y, WIN_TITLE_MAX_X, 0, 0);
+    userWin = createSubWindow(stdscr, WIN_USER_MAX_Y, WIN_USER_MAX_X, WIN_TITLE_MAX_Y + 1, 0);
+    lineWin = createSubWindow(stdscr, WIN_LINENUM_MAX_Y, WIN_LINENUM_MAX_X, WIN_TITLE_MAX_Y + 1, WIN_USER_MAX_X + 1);
+    editorWin = createSubWindow(stdscr, WIN_EDITOR_MAX_Y, WIN_EDITOR_MAX_X, WIN_TITLE_MAX_Y + 1, WIN_LINENUM_MAX_X + WIN_USER_MAX_X + 2);
+
+    configureWindow(userWin, COLOR_PAIR(3));
+    configureWindow(lineWin, COLOR_PAIR(3));
+    configureWindow(editorWin, COLOR_PAIR(2));
+    configureWindow(titleWin, COLOR_PAIR(1));
+
+    writeTitle();
+    writeLineNumbers();
+    writeUser("RicardoB", 4);
+
+    wmove(editorWin, 0, 0);
+    wrefresh(stdscr);
+    wrefresh(editorWin);
+
+    char* doc[WIN_EDITOR_MAX_Y] = {
+        "Ola tudo fixe isto e o documento brutal",
+        "vai ter duas linhas e ja e bem bom",
+        "oops afinal tem mais, isto e so um teste!",
+        " "
+    };
+
+    clearEditor(WIN_EDITOR_MAX_Y, WIN_EDITOR_MAX_X);
+    writeDocument(doc, 3);
+    wrefresh(editorWin);
+
+    int key;
+    int x = 0, y = 0;
+    mvwprintw(stdscr, 19, 0, "Em modo de navegacao");
+    refreshCursor(y, x);
+
+    while ((key = getch()) != 27) {
+        switch (key) {
+            case KEY_LEFT:
+                if (x > 0)
+                    x--;
+                break;
+            case KEY_RIGHT:
+                if (x < WIN_EDITOR_MAX_X)
+                    x++;
+                break;
+            case KEY_UP:
+                if (y > 0)
+                    y--;
+                break;
+            case KEY_DOWN:
+                if (y < WIN_EDITOR_MAX_Y)
+                    y++;
+                break;
+            case 10:
+                //TODO SE LINHA ESTÁ LIVRE, COLOCA OCUPADA E COMEÇA EDIÇÃO
+                editMode(y, x);
+                // TODO DESOCUPA A LINHA
+                mvwprintw(stdscr, 19, 0, "Em modo de navegacao");
+                break;
+        }
+        refreshCursor(y, x);
+    }
+    endwin();
+    return;
+}
+
+void editMode(int y, int x) {
+    int key;
+    char linha[WIN_EDITOR_MAX_X], linhaTemp[WIN_EDITOR_MAX_X];
+    getLinha(linha,y);
+    strncpy(linhaTemp, linha, WIN_EDITOR_MAX_X);
+    mvwprintw(stdscr, 19, 0, "Em modo de edicao   ");
+    refreshCursor(y, x);
+    while ((key = getch()) != 27) {
+        switch (key) {
+            case KEY_LEFT:
+                if (x > 0)
+                    x--;
+                break;
+            case KEY_RIGHT:
+                if (x < WIN_EDITOR_MAX_X)
+                    x++;
+                break;
+            case KEY_UP:
+                break;
+            case KEY_DOWN:
+                break;
+            case 10:
+                return;
+            case KEY_BACKSPACE:
+                //apaga = 1;
+                //backSpaceKey(posy, posx, ncol);
+                //moveLeft(&posx, posy);
+                break;
+            default:
+                writeKey(key, linha, x);
+                writeTextLine(linha, y);
+                break;
+        }
+        refreshCursor(y, x);
+    }
+}
+
+WINDOW * createSubWindow(WINDOW* janelaMae, int dimY, int dimX, int startY, int startX) {
+    return subwin(janelaMae, dimY, dimX, startY, startX);
+}
+
+void configureWindow(WINDOW* janela, int setCores) {
+    wattrset(janela, setCores);
+    wbkgd(janela, setCores);
+    wclear(janela);
+    wrefresh(janela);
+}
+
+void writeTitle() {
+    mvwprintw(titleWin, 0, 0, "%s", TITLE);
+}
+
+void writeLineNumbers() {
+    int i;
+
+    for (i = 0; i < WIN_LINENUM_MAX_Y; i++) {
+        mvwprintw(lineWin, i, 0, "%02d", i);
+    }
+}
+
+void writeUser(char* name, int line) {
+    mvwprintw(userWin, line, 0, "%s", name);
+}
+
+void writeDocument(char* text[], int nLines) {
+    int i;
+    for (i = 0; i < nLines; i++)
+        writeTextLine(text[i], i);
+}
+
+void writeTextLine(char* text, int line) {
+    mvwprintw(editorWin, line, 0, "%s", text);
+    wrefresh(editorWin);
+}
+
+void clearEditor(int dimY, int dimX) {
+    int i;
+    for (i = 0; i < dimY; i++) {
+        resetLine(i, dimX);
+    }
+}
+
+void resetLine(int line, int dimX) {
+    for (int i = 0; i < dimX; i++)
+        mvwprintw(editorWin, line, i, " ");
+}
+
+void refreshCursor(int y, int x) {
+    int cy, cx;
+    wmove(editorWin, y, x);
+    getyx(editorWin, cy, cx);
+    mvwprintw(stdscr, 20, 0, "l: %02d\tc: %02d", cy, cx);
+    wrefresh(stdscr);
+    wrefresh(editorWin);
+}
+
+int moveAllToTheRight(char* linha, int x) {
+    if (linha[WIN_EDITOR_MAX_X] != ' ')
+        return 0;
+    int max=WIN_EDITOR_MAX_X;
+    for (; max > x; max--) 
+        linha[max] = linha[max-1];
+    return 1;
+}
+
+void writeKey(int key, char* linha, int x) {
+    if(moveAllToTheRight(linha, x))
+        linha[x]=key;
+}
+
+void getLinha(char* linha, int y) {
+    int i;
+    for (i = 0; i <= WIN_EDITOR_MAX_X; i++) {
+        linha[i] = mvwinch(editorWin, y, i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* OLD VERSION OF THE WORK */
+
+/**
+ * Função responsável por tudo acerca do editor.
+ */
+/*void editor() {
     int nrow, ncol, posx, posy, oposx, oposy;
     int ch;
     char cursor = 219, tempChar;
@@ -129,7 +404,7 @@ void editor() {
  * @param ncol Número de colunas.
  * @param cursor Caractér que representa o cursor.
  */
-void editMode(char* text, int posx, int posy, int ncol, char cursor) {
+/*void editMode(char* text, int posx, int posy, int ncol, char cursor) {
     int ch, oposx, apaga = 0;
     char temp[DEFAULT_MAXCOLUMNS], tempChar;
     mvprintw(0, 0, "Modo de Edicao   ");
@@ -186,7 +461,7 @@ void editMode(char* text, int posx, int posy, int ncol, char cursor) {
  * Esta função inicializa as linhas.
  * @param text string para ser inicializada
  */
-void resetLine(char *text) {
+/*void resetLine(char *text) {
     for (int i = 0; i < 45; i++)
         text[i] = ' ';
 }
@@ -195,26 +470,26 @@ void resetLine(char *text) {
  * Esta função enumera as linhas do editor.
  * @param rows quantidade de linhas a enumerar.
  */
-void preencheLinhas(int rows) {
+/*void preencheLinhas(int rows) {
     for (int i = 1; i <= rows; i++) {
         mvprintw(i, 0, "%02d - ", i - 1);
     }
 }
 
 void moveUp(int *posy) {
-    *posy = (*posy > 0) ? *posy - 1 : *posy;
+ *posy = (*posy > 0) ? *posy - 1 : *posy;
 }
 
 void moveDown(int *posy, int nrow) {
-    *posy = (*posy < (nrow - 1)) ? *posy + 1 : *posy;
+ *posy = (*posy < (nrow - 1)) ? *posy + 1 : *posy;
 }
 
 void moveLeft(int *posx, int posy) {
-    *posx = (*posx > 0) ? *posx - 1 : posy;
+ *posx = (*posx > 0) ? *posx - 1 : posy;
 }
 
 void moveRight(int *posx, int ncol) {
-    *posx = (*posx < (ncol - 1)) ? *posx + 1 : *posx;
+ *posx = (*posx < (ncol - 1)) ? *posx + 1 : *posx;
 }
 
 /**
@@ -230,7 +505,7 @@ void moveRight(int *posx, int ncol) {
  * @param ncol Número de colunas.
  * @param nav 1 caso esteja em modo navegação e 0 caso contrário.
  */
-void changeCursorPosition(int posx, int oposx, int posy, int oposy, char cursor, char tempChar, int ncol, int nav, int apaga) {
+/*void changeCursorPosition(int posx, int oposx, int posy, int oposy, char cursor, char tempChar, int ncol, int nav, int apaga) {
     if (!nav) mvprintw(0, 0, "Modo de Edicao   ");
     else mvprintw(0, 0, "Modo de Navegacao");
     mvprintw(0, ncol - 20, "Cursor: (%d, %d)  ", posy, posx);
@@ -250,7 +525,7 @@ void changeCursorPosition(int posx, int oposx, int posy, int oposy, char cursor,
  * @param posx Posição atual no eixo dos X.
  * @param ncol Número de colunas.
  */
-void backSpaceKey(int posy, int posx, int ncol) { //TODO CONSERTAR BUG: Está a manter o caracter onde tava o cursor!
+/*void backSpaceKey(int posy, int posx, int ncol) { //TODO CONSERTAR BUG: Está a manter o caracter onde tava o cursor!
     char tempChar;
     for (; posx < ncol - 1; posx++) {
         tempChar = mvinch(posy, posx + 1);
@@ -267,7 +542,7 @@ void backSpaceKey(int posy, int posx, int ncol) { //TODO CONSERTAR BUG: Está a 
  * @param posx Posição atual no eixo dos X.
  * @param ncol Número de colunas.
  */
-void deleteKey(int posy, int posx, int ncol) { //TODO CONSERTAR BUG: Não está a funcionar como devia
+/*void deleteKey(int posy, int posx, int ncol) { //TODO CONSERTAR BUG: Não está a funcionar como devia
     char tempChar;
     mvaddch(posy, ncol, ' ');
     for (; ncol > posx; ncol--) {
@@ -286,7 +561,7 @@ void deleteKey(int posy, int posx, int ncol) { //TODO CONSERTAR BUG: Não está 
  * @param ncol Número de colunas.
  * @return 1 se foi possivel, 0 caso contrário.
  */
-int moveAllToTheRight(int posy, int posx, int ncol) {
+/*int moveAllToTheRight(int posy, int posx, int ncol) {
     if (mvinch(posy, ncol - 1) != ' ')
         return 0;
     char tempChar;
@@ -302,3 +577,4 @@ void getTextoDaLinha(char* texto, int linha, int maxX) {
     for (int i = 0; i < maxX; i++)
         texto[i] = mvinch(linha, i + X_INDEX);
 }
+ */
--- Cliente/client-functions.h
+++ Cliente/client-functions.h
@@ -3,5 +3,5 @@
 void checkArgs(int argc, char** argv);
 void loginSession();
 void editor();
-void editMode(char* text, int posx, int posy, int ncol, char cursor);
+void editMode(int y, int x);
 #endif /* CLIENT_FUNCTIONS_H */
