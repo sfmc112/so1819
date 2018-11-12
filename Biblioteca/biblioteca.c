c9a1c17dea9c3017655906e3ef7bbcc18805c1a0
sfmc112 <missntc.sf@gmail.com>
2018-11-12 16:04:11 +0000

    Projeto biblioteca
    criei projecto biblioteca que vai ter funções comuns ao servidor e ao
    cliente

---
+++ Biblioteca/biblioteca.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: sarah
+ *
+ * Created on November 12, 2018, 3:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
